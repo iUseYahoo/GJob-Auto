ce4bea8ca8c5ef0a48c179aca5001a3de74380cb
Cxstom <customdraws@gmail.com>
2023-06-08 13:35:49 +0200

    Humble Office refactor for a humble guy.

---
+++ src/main/java/data/ExampleOffice.java
@@ -0,0 +1,12 @@
+package data;
+
+import gearth.extensions.parsers.HGender;
+
+public class ExampleOffice extends OfficeData {
+    public ExampleOffice(int roomId, int groupId, String mottoTag) {
+        super(roomId, groupId, mottoTag);
+
+        this.appendFigure(HGender.Male, "ca-1813-0.sh-300-64.ch-225-73.lg-285-64.hd-180-1");
+        this.appendFigure(HGender.Female, "ca-1813-0.sh-907-64.hd-600-1.lg-715-64.hr-515-33.ch-880-73");
+    }
+}
